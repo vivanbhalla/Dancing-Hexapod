abfa4b829f2a71815de82d8ad0e1b2148e4d81fb
Samuel K Burkhart <samuel.burkhart@gmail.com>
2017-12-10 19:54:02 -0800

    Added demo video of hexapod functionality and script option for client interface

--- hexapod/client_if.py
+++ hexapod/client_if.py
@@ -15,22 +15,37 @@ def get_args():
             type=int,
             help='port used to communicate to the hexapod')
 
+    parser.add_argument(
+            '-s',
+            '--script',
+            default=None,
+            help='script file to run robot')
+
     return parser.parse_args()
 
-def Main(host, port):
+def Main(host, port, script):
         mySocket = socket.socket()
         mySocket.connect((host,port))
-         
-        message = input(" -> ")
-         
-        while message != 'q':
-            if message:
-                mySocket.send(message.encode())
-                data = mySocket.recv(1024).decode()
-                 
-                print ('Received from server: ' + data)
-                 
+
+        if script:
+            with open(script) as f:
+                for line in f:
+                    mySocket.send(line.strip().encode())
+                    data = mySocket.recv(1024).decode()
+
+                    print('Recieved from server: ' + data)
+                    
+        else:
             message = input(" -> ")
+             
+            while message != 'q':
+                if message:
+                    mySocket.send(message.encode())
+                    data = mySocket.recv(1024).decode()
+                     
+                    print ('Received from server: ' + data)
+                     
+                message = input(" -> ")
                  
         mySocket.close()
  
@@ -39,5 +54,7 @@ if __name__ == '__main__':
 
     print('Host: {}'.format(args.host))
     print('Port: {}'.format(args.port))
+    print('Script: {}'.format(args.script))
 
-    Main(args.host, args.port)
+    Main(args.host, args.port, args.script)
+
